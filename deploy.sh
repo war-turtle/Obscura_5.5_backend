814bbe9fc67cebeb25a25cd7a4435519cebf3040
war-turtle <kartikyadav71199@gmail.com>
2019-02-28 01:37:59 +0530

    pushing to cluster

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-# sudo snap install kubectl --classic
-# pip install awscli
-# wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
-# chmod +x kops-linux-amd64
-# sudo mv kops-linux-amd64 /usr/local/bin/kops
-# kops get cluster
-# kops export kubecfg --name ${KOPS_CLUSTER_NAME}
+sudo snap install kubectl --classic
+pip install awscli
+wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+kops get cluster
+kops export kubecfg --name ${KOPS_CLUSTER_NAME}
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build --build-arg pass_phrase=$PASS_PHRASE -t warturtle/obscura6-backend:latest -t warturtle/obscura6-backend:$SHA .
 docker push warturtle/obscura6-backend:latest
 docker push warturtle/obscura6-backend:$SHA
 
-# kubectl apply -f kubernetes
-# kubectl set image deployment/backend-deployment server=warturtle/obscura6-backend:$SHA
+kubectl apply -f kubernetes
+kubectl set image deployment/backend-deployment server=warturtle/obscura6-backend:$SHA
