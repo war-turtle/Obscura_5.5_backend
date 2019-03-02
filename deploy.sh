20fd356ccdefd7ad3f50436c3f4cd8f682ad10fa
war-turtle <kartikyadav71199@gmail.com>
2019-03-02 20:08:19 +0530

    little change in deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ docker push warturtle/obscura6-backend:latest
 docker push warturtle/obscura6-backend:$SHA
 
 gpg --batch --yes --passphrase ${PASS_PHRASE} -o obscura.pem -d obscura.pem.gpg
+sudo chmod 400 obscura.pem
 ssh -o "StrictHostKeyChecking no" -i obscura.pem ubuntu@www.obscuranitkkr.co.in sudo docker service update --image warturtle/obscura6-backend:$SHA backend
 # kubectl apply -f kubernetes
 # kubectl set image deployment/backend-deployment backend=warturtle/obscura6-backend:$SHA
